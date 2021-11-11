d5e1c129db4007d43324faadaaadacad5194f139
Kamesh <kameshkumar4602@gmail.com>
2021-11-11 22:28:19 +0530

    server setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+
 function App() {
   return (
-    <>
-      <h1>Welcome</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Container className="py-3">
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 

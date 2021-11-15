f39dd08b746706b6d31d6d91dd02523057ab4be5
Kamesh <kameshkumar4602@gmail.com>
2021-11-15 22:54:18 +0530

    Loader Msg HomeScreen

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import Product from '../models/productModel.js'
 //get all products
 router.get('/', asyncHandler(async(req, res) => {
     const products = await Product.find({})
+
     res.json(products)
 
 }))

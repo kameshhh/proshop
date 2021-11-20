de07e758577e0ff4050ebe02d2d7960d25c0e8fc
Kamesh <kameshkumar4602@gmail.com>
2021-11-20 22:47:42 +0530

    Header

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants"
 
 
 export const login = (email, password) => async(dispatch) => {
@@ -32,4 +32,16 @@ export const login = (email, password) => async(dispatch) => {
                 error.response.data.message : error.message,
         })
     }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+        // localStorage.removeItem('cartItems')
+        // localStorage.removeItem('shippingAddress')
+        // localStorage.removeItem('paymentMethod')
+    dispatch({ type: USER_LOGOUT })
+        // dispatch({ type: USER_DETAILS_RESET })
+        // dispatch({ type: ORDER_LIST_MY_RESET })
+        // dispatch({ type: USER_LIST_RESET })
+        // document.location.href = '/login'
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Route } from "react-router-dom";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+    // console.log("logout");
+  };
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -18,11 +31,22 @@ const Header = () => {
                   <i className="fas fa-shopping-cart px-1"></i>Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <i className="fas fa-user px-1"></i>Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-user"></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>

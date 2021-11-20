ed33d23b54f4f8e618486b2e20b3ac8c3effcb82
Kamesh <kameshkumar4602@gmail.com>
2021-11-20 17:42:41 +0530

    user controller bug

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,8 +27,23 @@ const authUser = asyncHandler(async(req, res) => {
 
 // get user prifile
 const getUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
 
-    res.send('success')
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+
+    }
+
+
+
 })
 
 export { authUser, getUserProfile }

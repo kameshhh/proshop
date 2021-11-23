d0305b351ffb22599de7d466f234729600962ec1
Kamesh <kameshkumar4602@gmail.com>
2021-11-23 20:33:47 +0530

    order state and orderScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderSreen from "./screens/PlaceOrderSreen";
+import OrderScreen from "./screens/OrderScreen";
 
 function App() {
   return (
@@ -26,6 +27,8 @@ function App() {
           <Route path="/shipping" component={ShippingScreen} />
           <Route path="/payment" component={PaymentScreen} />
           <Route path="/placeorder" component={PlaceOrderSreen} />
+          <Route path="/order/:id" component={OrderScreen} />
+
           <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
